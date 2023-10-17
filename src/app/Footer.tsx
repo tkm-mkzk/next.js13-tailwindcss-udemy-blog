55988e466f2ce2ce5858608cf15f71792458e5b3
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-10-18 08:11:23 +0900

    first commit

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="py-2 px-4 border-">
+      <small>@2023 mikkacode</small>
+    </footer>
+  )
+}
+
+export default Footer
