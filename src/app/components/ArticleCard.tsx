be752f7ea11c17322abd3facecd65d000677904c
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-10-21 08:05:45 +0900

    supabaseの設定追加

--- src/app/components/ArticleCard.tsx
+++ src/app/components/ArticleCard.tsx
@@ -28,7 +28,9 @@ const ArticleCard: React.FC<ArticleCardProps> = ({ article }) => {
         >
           {article.title}
         </Link>
-        <p className="text-sm pb-3 text-slate-900">published on {article.createdAt}</p>
+        <p className="text-sm pb-3 text-slate-900">
+          published on {new Date(article.createdAt).toLocaleString()}
+        </p>
         <Link href={`articles/${article.id}`} className="text-slate-900 pb-6">
           {article.content.length > 70
             ? article.content.substring(0, 70)
